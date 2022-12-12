6c973faf460be870c87b2c0962f88f40db0f56db
hitpakdut.newlikud <hitpakdut.newlikud@gmail.com>
2022-12-12 20:59:08 +0200

    add meta Tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 // https://github.com/mzuccaroli/angular-google-tag-manager/blob/master/demo-application/src/app/app.component.ts
 import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
+import { Meta } from '@angular/platform-browser';
+//import { CanonicalService } from './canonical.service';
+
 
 //import { GoogleTagManagerService } from 'angular-google-tag-manager';
 
@@ -12,6 +15,8 @@ import { NavigationEnd, Router } from '@angular/router';
 export class AppComponent implements OnInit {
   constructor(
     private router: Router,
+    private meta:Meta,
+    //private canonical: CanonicalService
     //private gtmService: GoogleTagManagerService,
   ) {
   }
@@ -43,5 +48,13 @@ export class AppComponent implements OnInit {
        // this.gtmService.pushTag(gtmTag);
       }
     });
+    this.meta.addTags([
+      { charset: 'UTF-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'date', content: '2022-12-05', scheme: 'YYYY-MM-DD' },
+      { name: 'keywords', content: 'מיזם מסעדה פתוחה מנגיש את נתוני דוחות התברואה שבוצעו לבתי אוכל בישראל' },
+      { name: 'author', content: 'Alon Antin' },
+      { name: 'robots', content: 'index, follow' }
+    ]);     
   }
 }
