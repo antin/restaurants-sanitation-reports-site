80ac7d3984918a0b91d07f91bd28e2694392c822
hitpakdut.newlikud <hitpakdut.newlikud@gmail.com>
2023-01-23 23:14:45 +0200

    add modal dialog at first use

--- src/app/features/restaurants-list/pages/restaurants-list.component.ts
+++ src/app/features/restaurants-list/pages/restaurants-list.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
-import {UntypedFormGroup} from '@angular/forms';
-import {RestaurantsListService} from '../services/restaurants-list.service';
+import { Component, OnInit, ViewChild, AfterViewInit, Inject } from '@angular/core';
+import { UntypedFormGroup } from '@angular/forms';
+import { RestaurantsListService } from '../services/restaurants-list.service';
 //import { CDK_DESCRIBEDBY_ID_PREFIX } from '@angular/cdk/a11y';
 
 // https://material.io/resources/icons/?icon=format_list_bulleted&style=baseline
@@ -16,25 +16,31 @@ import { restaurant } from '../models/restaurant.model';
 //import { searchValueFilterPipe } from './search-value-filter-pipe';
 import { orestaurantHeaderComponent } from 'src/app/features/restaurants-list/components/ORestaurantHeaderComponent';
 import { orestaurantsListComponent } from 'src/app/features/restaurants-list/components/ORestaurantsListComponent';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 declare var gtag: any;
 
 @Component({
   selector: 'app-restaurants-list',
   templateUrl: './restaurants-list.component.html',
-  styleUrls: ['./restaurants-list.component.css'],  
+  styleUrls: ['./restaurants-list.component.css'],
 })
 export class RestaurantsListComponent implements AfterViewInit {
 
+  //public static hasAdvertBeenShown = MyComponent.hasAdvertBeenShownBefore()
+  private storage: Storage = localStorage // Change this based on your use-case
+
+  hasDisclaimerBeenShown = this.hasDisclaimerBeenShownBefore();
+  
   // if service has Errors
- hasError = false;
+  hasError = false;
 
   // if service has Errors show them
- errorMsg : string;
-  
- 
-   // if service has   searchValue filter by it throw name and city
-   searchValue : string = "";
+  errorMsg: string;
+
+
+  // if service has   searchValue filter by it throw name and city
+  searchValue: string = "";
 
   footerLinks = [
     {
@@ -71,24 +77,10 @@ export class RestaurantsListComponent implements AfterViewInit {
     }
   ];
 
- //https://stackoverflow.com/questions/63565926/how-to-fix-cannot-read-property-todataurl-of-undefined-in-ngx-signaturepad
+  //https://stackoverflow.com/questions/63565926/how-to-fix-cannot-read-property-todataurl-of-undefined-in-ngx-signaturepad
   // for convenience as we don't have a QueryList.index
   //https://github.com/lathonez/angular2-signaturepad-demo/blob/master/src/app/app.component.ts
 
-
-  //@ViewChild('stepper', { static: false }) private myStepper: MatStepper;
-
-  /*
-  personEssentialDetailsFormGroup = new FormGroup({
-    cellular : new FormControl('', [Validators.required, Validators.pattern('[0-9]{10}')]),
-    email : new FormControl('', [Validators.required, Validators.email]),    
-  });
- */
-  /*
-  promiseRestaurants: Promise<restaurant[]>;
-  public service: RestaurantsListService;
-  restaurants: restaurant[] = [];
-  */
   errorMessage = '';
   data: any;
   EmpForm: UntypedFormGroup;
@@ -100,46 +92,69 @@ export class RestaurantsListComponent implements AfterViewInit {
   zoom: any;
   origin: any;
   destination: any
-  
+
+  animal: string;
+  name: string;
+
   constructor(private ServiceService: RestaurantsListService,
-    //private _formBuilder: FormBuilder,
+    public dialog: MatDialog,
     private activatedRoute: ActivatedRoute,
-    ) {
- 
-    }
+  ) {
 
-    ngOnInit() {
-      this.getUserLocation();
-   }
+  }
 
-   getUserLocation() 
-   {
-      if (navigator.geolocation) 
-      {
-        // use api to find the current city? https://gps-coordinates.org/
-       navigator.geolocation.getCurrentPosition(position => {
-           this.lat = position.coords.latitude;
-           this.lng = position.coords.longitude;
-           console.log("User allow,this.lat:" + this.lat + " ;this.lng:" + this.lng)
-         });
-      }
-      else 
-      {
-        console.log("User not allow")
-      }
+  ngOnInit() {
+    this.getUserLocation();
+    if(!this.hasDisclaimerBeenShown) {      
+      this.openDisclaimerDialog();
+      this.markDisclaimerAsSeen()
     }
-   
-    searchTermChanged(term: string) {
-      console.log('STC', term);
-      this.searchValue = term;
-      
-    }
+  }
 
-    public ngAfterViewInit() {
-      this.beResponsive();
-      
+  public hasDisclaimerBeenShownBefore() : boolean {
+    return JSON.parse(localStorage.getItem('disclaimer'))
+  }
+  public markDisclaimerAsSeen() : void {
+    localStorage.setItem('disclaimer',  'true')
+  }
+  
+  openDisclaimerDialog(): void {
+    let dialogRef = this.dialog.open(DialogOverviewDisclaimerDialog, {
+      width: '650px',
+      data: { name: this.name, animal: this.animal }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
+  }
+
+  getUserLocation() {
+    if (navigator.geolocation) {
+      // use api to find the current city? https://gps-coordinates.org/
+      navigator.geolocation.getCurrentPosition(position => {
+        this.lat = position.coords.latitude;
+        this.lng = position.coords.longitude;
+        console.log("User allow,this.lat:" + this.lat + " ;this.lng:" + this.lng)
+      });
     }
-    
+    else {
+      console.log("User not allow")
+    }
+  }
+
+  searchTermChanged(term: string) {
+    console.log('STC', term);
+    this.searchValue = term;
+
+  }
+
+  public ngAfterViewInit() {
+    this.beResponsive();
+
+  }
+
 
   nextClicked(event: any) {
     // complete the current step
@@ -159,7 +174,7 @@ export class RestaurantsListComponent implements AfterViewInit {
   // set the dimensions of the signature pad canvas
   public beResponsive() {
     console.log('Resizing signature pad canvas to suit container size');
-   
+
   }
 
   public clear() {
@@ -167,24 +182,24 @@ export class RestaurantsListComponent implements AfterViewInit {
 
   }
 
- 
 
-  
+
+
 
   // https://medium.com/@ole.ersoy/custom-google-analytics-events-with-angular-214cbf954130
-  
 
-      SendAddToCartEvent(){
-        //  this.googleAnalyticsService.eventEmitter("add_to_cart", "shop", "cart", "click", 10);
-      }
+
+  SendAddToCartEvent() {
+    //  this.googleAnalyticsService.eventEmitter("add_to_cart", "shop", "cart", "click", 10);
+  }
 
   Save(step: any) {
     this.submitted = true;
-     
-        // https://stackoverflow.com/questions/60508242/pass-the-data-to-next-step-in-component-based-mat-stepper
-    
 
-     
+    // https://stackoverflow.com/questions/60508242/pass-the-data-to-next-step-in-component-based-mat-stepper
+
+
+
 
   }
 
@@ -192,19 +207,34 @@ export class RestaurantsListComponent implements AfterViewInit {
     this.hasError = true;
 
     let errorMessage = '';
-    if (error.error != undefined && error.error.length != undefined && error.error.length > 0 ) {
-    //  if (error.error instanceof ErrorEvent) {
-        // client-side error
-        errorMessage = `פירוט השגיאה: ${error.error}`;
-        this.errorMsg = errorMessage;
+    if (error.error != undefined && error.error.length != undefined && error.error.length > 0) {
+      //  if (error.error instanceof ErrorEvent) {
+      // client-side error
+      errorMessage = `פירוט השגיאה: ${error.error}`;
+      this.errorMsg = errorMessage;
     } else {
-        // server-side error
-        errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+      // server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
     }
     console.log(errorMessage);
     //this.errorMsg = errorMessage;
     return throwError(errorMessage);
+  }
+
 }
 
+@Component({
+  selector: 'dialog-overview-disclaimer-dialog',
+  templateUrl: './dialog-overview-disclaimer-dialog.html',
+})
+export class DialogOverviewDisclaimerDialog {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewDisclaimerDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 
 }
